210fe69a91c9e1673077d51f9f234a4adcdfdefd
niharika-1999 <niharika.rao@ymedialabs.com>
2021-08-26 12:27:26 +0530

    Added:ShowBoard to display current board

--- src/com/bridgelabz5/TicTacToe.java
+++ src/com/bridgelabz5/TicTacToe.java
@@ -3,7 +3,9 @@ import java.util.Scanner;
 
 public class TicTacToe {
 	public static char[] board = new char[10];//array of size 10
+	private char[][] showboard = new char[3][3];
 	char CompChoice,PlayerChoice;
+	int i,j;
 	public void createBoard() //Initializing the board
 	{
 		for(int i=1;i<board.length;i++)
@@ -11,7 +13,7 @@ public class TicTacToe {
 			board[i]=' ';
 		}
 	}
-	public void chooseLetter()
+	public void chooseLetter()//Choosing letter x or o by player
 	{
 		System.out.println("Choose letter x or o");
 		Scanner sc=new Scanner(System.in);
@@ -31,11 +33,31 @@ public class TicTacToe {
 		
 	}
 	
+	
+	public void showBoard()//showBoard to display current board
+	{
+		for(int i =0;i<3;i++) {
+			for(int j=0;j<3;j++) {
+				showboard[i][j]=' ';
+				
+			}
+		}
+		System.out.println(showboard[0][0]+"|"+showboard[0][1]+"|"+showboard[0][2]);
+		System.out.println("-----");
+		System.out.println(showboard[1][0]+"|"+showboard[1][1]+"|"+showboard[1][2]);
+		System.out.println("-----");
+		System.out.println(showboard[2][0]+"|"+showboard[2][1]+"|"+showboard[2][2]);
+		
+	}
+	
+		
+	
 	public static void main(String[] args) {
 		System.out.println("Welcome to Tic Tac Toe");//Welcome message
 		TicTacToe game=new TicTacToe();
 		game.createBoard();
 		game.chooseLetter();
+		game.showBoard();
 	
 		}
 		
